e4aaf9ebe02fdfdadfabd1626bf04758e01f74a0
sarasumanth <69038432+sarasumanth@users.noreply.github.com>
2020-11-24 15:10:52 +0530

    Add files via upload

---
+++ perl 81.pl
@@ -0,0 +1,17 @@
+print "enter 2 numbers\n";
+$a = <STDIN>;
+$b = <STDIN>;
+$c = $a + $b;
+print 'Value of $a + $b = ' . $c . "\n";
+
+$c = $a - $b;
+print 'Value of $a - $b = ' . $c . "\n";
+
+$c = $a * $b;
+print 'Value of $a * $b = ' . $c . "\n";
+
+$c = $a / $b;
+print 'Value of $a / $b = ' . $c . "\n";
+
+$c = $a % $b;
+print 'Value of $a % $b = ' . $c. "\n";
---
+++ perl 82.pl
@@ -0,0 +1,6 @@
+print("Displaying text \n in new line by using \\n. \n");
+print("Displaying text \t in new tab by using \\t. \n");
+print("\"Printing in double quotes.\"\n");
+print("\'Printing in single quotes.\'\n");
+print("\L printing text in lower case.\n");
+print("\U PRINTING TEXT IN UPPER CASE.\n");
